df6014a3f062483a72d7d7b11d5ecc89b23d2c12
Blue260910 <victorhugoaranda2609@gmail.com>
2023-06-02 15:22:44 -0300

    Responsividade quem somos

--- JS/home.js
+++ JS/home.js
@@ -1,9 +1,11 @@
-const Abrirlista = document.querySelector('.Menu_Esquerda_Simbulo');
+
+
+const Abrirlista = document.querySelector('.Menu_Esquerda_Aberto .Menu_Esquerda_Simbulo');
 
 Abrirlista.addEventListener('click', () => {
     console.log('Clicou');
-    if (document.querySelector('.Menu_Esquerda_Aberto ul').style.display != 'block') {
-        document.querySelector('.Menu_Esquerda_Aberto ul').style.display = 'block';
+    if (document.querySelector('.Menu_Esquerda_Aberto ul').style.display != 'flex') {
+        document.querySelector('.Menu_Esquerda_Aberto ul').style.display = 'flex';
         return;
     }else{
         document.querySelector('.Menu_Esquerda_Aberto ul').style.display = 'none';
