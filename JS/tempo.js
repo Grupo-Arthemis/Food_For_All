57192afe273977a9fd7296b0dc58c74cf1d7eb42
luanmacea <luanmacea@gmail.com>
2023-06-04 16:20:38 -0300

    adicionando data/arrumando bugs gerais

--- JS/tempo.js
+++ JS/tempo.js
@@ -1,11 +1,18 @@
 function exibirHoraAtual() {
     var campoHora = document.getElementById("horaAtual");
+    var campoData = document.getElementById("dataAtual");
     var dataAtual = new Date();
     var hora = formatarNumero(dataAtual.getHours());
     var minutos = formatarNumero(dataAtual.getMinutes());
     var segundos = formatarNumero(dataAtual.getSeconds());
     var horaAtual = hora + ":" + minutos + ":" + segundos;
     campoHora.innerText = horaAtual;
+
+    var dia = formatarNumero(dataAtual.getDate());
+    var mes = formatarNumero(dataAtual.getMonth() + 1);
+    var ano = dataAtual.getFullYear();
+    var dataAtualFormatada = dia + "/" + mes + "/" + ano;
+    campoData.innerText = dataAtualFormatada;
 }
 
 function formatarNumero(numero) {
